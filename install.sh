231efe406ab0a70ef36b13cbd736476bf3a72fee
Sébastien Theys <seb@odoo.com>
2018-06-16 00:42:20 +0200

    [IMP] Added useful files

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -z "$1" ]
+	then
+		REPO="${DIR}/.."
+	else
+		REPO="$1"
+fi
+
+ln -s "${DIR}/.odoorc-dev" ~/.odoorc
+ln -s "${DIR}/.gitconfig" ~/.gitconfig
+ln -s "${DIR}/.eslintrc" ~/.eslintrc
+ln -s "${DIR}/.odoorc-dev" "${REPO}/.odoorc"
+ln -s "${DIR}/.flake8" "${REPO}/.flake8"
+ln -s "${DIR}/odoo.sublime-project" "${REPO}/odoo.sublime-project"
