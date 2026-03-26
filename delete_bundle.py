98ee8af295ed3bffc6744b25328cd4d78e26d911
Sébastien Theys <seb@odoo.com>
2026-03-26 17:10:25 +0100

    [IMP] command_runner, delete_bundle, fetch_bundle: use threads

--- delete_bundle.py
+++ delete_bundle.py
@@ -1,13 +1,16 @@
 """Fully delete a bundle locally."""
 
+from concurrent.futures import ThreadPoolExecutor
 from rich import print
+from rich.live import Live
+from rich.tree import Tree
 
 import argparse
 import glob
-import subprocess
-import time
 
-start_time = time.perf_counter()
+from command_runner import ignore_error, PrintParams, Runner
+
+runner = Runner()
 
 ROOT = "/home/seb/repo"
 repos = ["design-themes", "documentation", "enterprise", "odoo", "upgrade-util", "upgrade"]
@@ -28,64 +31,66 @@ parts = bundle_name.split("-")
 base = f"{parts[0]}-{parts[1]}" if parts[0] == "saas" else parts[0]
 wt_root_folder = f"/home/seb/src/odoo/{base}/{bundle_name}"
 
-def run(cmd, *, cwd=None, handle_exceptions=None, **kwargs):
-    elapsed_time = time.perf_counter() - start_time
-    print(f"{elapsed_time:.2f} [yellow]From: " + (cwd or "current folder") + "[/yellow]")
-    state = "✅️"
-    error = None
-    try:
-        subprocess.run(cmd, cwd=cwd, check=True, text=True, capture_output=True, **kwargs)
-    except subprocess.CalledProcessError as e:
-        if handle_exceptions:
-            for msg, handler in handle_exceptions.items():
-                if msg in e.stderr:
-                    handler()
-                    state = "⚠️"
-                    return
-        state = "❌"
-        error = e.stderr
-        raise
-    finally:
-        print(state + " " + " ".join(cmd))
-        if error:
-            print(error)
 
-for repo in repos:
+def handle_repo(repo, live, tree):
     repo_folder = f"{ROOT}/{repo}"
+    current_tree = tree.add(repo)
+    live.refresh()
     repo_wt_root_folder = f"{wt_root_folder}/{repo}"
-    run(
+    runner.run(
         ["git", "worktree", "remove", repo_wt_root_folder],
         cwd=repo_folder,
+        print_params=PrintParams(live, current_tree),
         handle_exceptions={
-            f"fatal: '{repo_wt_root_folder}' is not a working tree": lambda: None,
+            f"fatal: '{repo_wt_root_folder}' is not a working tree": ignore_error,
         },
     )
-    run(
+    runner.run(
         ["git", "update-ref", "-d", f"refs/remotes/{remote_dev_by_repo[repo]}/{bundle_name}"],
         cwd=repo_folder,
+        print_params=PrintParams(live, current_tree),
     )
-    run(
+    runner.run(
         ["git", "branch", "-D", bundle_name],
         cwd=repo_folder,
-        handle_exceptions={f"error: branch '{bundle_name}' not found": lambda: None},
+        print_params=PrintParams(live, current_tree),
+        handle_exceptions={f"error: branch '{bundle_name}' not found": ignore_error},
     )
-run(["rm", "-rf", wt_root_folder])
+
+
+tree = Tree("Repositories")
+with Live(tree, auto_refresh=False) as live:
+    futures = []
+    with ThreadPoolExecutor(max_workers=12) as executor:
+        for repo in repos:
+            futures.append(executor.submit(handle_repo, repo, live, tree))
+    for future in futures:
+        future.result()
+    live.refresh()
+
+runner.run(["rm", "-rf", wt_root_folder])
 
 path_pattern = f"/home/seb/.local/share/Odoo/filestore/{bundle_name}*"
 matching_files = glob.glob(path_pattern)
 
-for matching_file in matching_files:
-    if not matching_file.startswith(f"/home/seb/.local/share/Odoo/filestore/{bundle_name}"):
-        print(f"[red]Skipping {matching_file} as it doesn't match the expected pattern[/red]")
-        continue
-    run(["rm", "-rf", matching_file])
-    last_part = matching_file.split("/")[-1]
-    run(
-        ["dropdb", last_part],
-        handle_exceptions={
-            f'ERROR:  database "{last_part}" does not exist': lambda: None,
-            f'dropdb: error: database removal failed: ERROR:  database "{last_part}" does not exist': lambda: None,
-        },
-    )
+tree = Tree("Files")
+with Live(tree, auto_refresh=False) as live:
+    for matching_file in matching_files:
+        current_tree = tree.add(matching_file)
+        live.refresh()
+        if not matching_file.startswith(f"/home/seb/.local/share/Odoo/filestore/{bundle_name}"):
+            print(f"[red]Skipping {matching_file} as it doesn't match the expected pattern[/red]")
+            continue
+        runner.run(["rm", "-rf", matching_file], print_params=PrintParams(live, current_tree))
+        last_part = matching_file.split("/")[-1]
+        runner.run(
+            ["dropdb", last_part],
+            print_params=PrintParams(live, current_tree),
+            handle_exceptions={
+                f'ERROR:  database "{last_part}" does not exist': ignore_error,
+                f'dropdb: error: database removal failed: ERROR:  database "{last_part}" does not exist': ignore_error,
+            },
+        )
+    live.refresh()
 
 print("[green]Done[/green]")
